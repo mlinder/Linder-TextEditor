680683d75e9d4da4761ddfbc71519f58768d6950
Marcus Linder <marcus.linder@svt.se>
2020-03-12 16:42:50 +0100

    Added basic Slate editor

--- src/components/LinderTextEditor.jsx
+++ src/components/LinderTextEditor.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
 
 import "./LinderTextEditor.css";
 
 const LinderTextEditor = () => {
-  return <div>Hi</div>;
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }]
+    }
+  ]);
+
+  return (
+    <div
+      className="text-editor"
+    >
+      <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+        <Editable />
+      </Slate>
+    </div>
+  );
 };
 
 export default LinderTextEditor;
