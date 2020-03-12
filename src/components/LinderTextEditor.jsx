b116600d5477a405a9e2b0efcf4b7bd1a71d549b
Marcus Linder <marcus.linder@svt.se>
2020-03-12 15:33:59 +0100

    Add placeholder component

---
+++ src/components/LinderTextEditor.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./LinderTextEditor.css";
+
+export const LinderTextEditor = () => {
+  return <div>Hi</div>;
+};
---
+++ src/components/LinderTextEditor.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { LinderTextEditor } from "./LinderTextEditor";
+
+export default {
+  title: "LinderTextEditor",
+  component: LinderTextEditor
+};
+
+export const Basic = () => <LinderTextEditor />;
